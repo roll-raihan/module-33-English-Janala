8ae88319cfafffd8787c35b571560cf090329f99
roll-raihan <raihanahmednasim@gmail.com>
2025-09-08 08:55:57 +0600

    finished english janala

--- script/index.js
+++ script/index.js
@@ -138,3 +138,18 @@ const displayLessons = (lessons) => {
 
 }
 loadLessons();
+
+//for search option
+document.getElementById("btn-search").addEventListener('click', () => {
+    removeActive();//when searched,remove active cls
+    const input = document.getElementById("input-search");
+    const searchValue = input.value.trim().toLowerCase();
+    // console.log(searchValue);
+    fetch("https://openapi.programming-hero.com/api/words/all")
+        .then(res => res.json())
+        .then(data => {
+            const allWords = data.data;
+            const filterWords = allWords.filter((word) => word.word.toLowerCase().includes(searchValue));
+            displayLevelWord(filterWords);
+        });
+});
